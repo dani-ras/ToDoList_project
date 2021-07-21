bb0d398067799d73a14449c0f9ac09e3fa9274d4
DanRas <danrasdev@gmail.com>
2021-07-21 11:06:12 +0300

    client-server br

---
+++ client/script.js
@@ -0,0 +1,76 @@
+printList()
+
+document.querySelector('form').onsubmit = event => {
+    event.preventDefault()
+
+    const values = Object.values(event.target)
+        .reduce((acc, input) => !input.name ? acc : ({
+            ...acc,
+            [input.name]: input.type == 'checkbox' ? input.checked : input.value
+        }), {}
+        )
+
+    axios.post('http:localhost:3000/task', values)
+        .then(res => {
+            printList()
+        })
+
+    document.querySelector('form').reset()
+}
+
+function printList() {
+    document.querySelector("#doLi").innerHTML = ""
+    document.querySelector("#doneLi").innerHTML = ""
+
+
+    axios.get('http:localhost:3000/task')
+        .then(res => {
+            const list = res.data
+
+            if (list) {
+                console.log(list);
+                list.forEach(element => {
+                    if (!element.checked) {
+                        document.querySelector("#doLi").innerHTML +=
+                            `<li> <label for=""> ${element.task} </label>
+                <span>
+                <input type="checkbox" onchange="trans_Li('${(element.id)}')">
+                <button onclick="removeItem('${element.id}')">x</button>
+            </span>
+            </li>`
+                    } else {
+                        document.querySelector("#doneLi").innerHTML +=
+                            `<li> <label for=""> ${element.task} </label>
+            <span>
+            <input type="checkbox" checked onchange="trans_Li('${element.id}')">
+            <button onclick="removeItem('${element.id}')">x</button>
+            </span>
+            </li>`
+                    }
+                });
+            }
+        })
+
+}
+
+// // // // // // // // // // // // // // // // // // // // // // // // // // 
+
+function trans_Li(id) {
+    console.log(id)
+    console.log({ "id": id })
+    axios.put('http:localhost:3000/task', { "id": id })
+        .then(res => {
+            printList()
+        }
+        )
+}
+
+function removeItem(id) {
+    console.log(id)
+
+    axios.delete(`http:localhost:3000/task/${id}`)
+        .then(res => {
+            printList()
+        }
+        )
+}
